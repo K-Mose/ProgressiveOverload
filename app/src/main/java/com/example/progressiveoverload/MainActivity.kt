18cda0f7cb531d82deb225806f5a24c2bcaba959
user <km_413@naver.com>
2021-09-22 02:50:42 +0900

    MainActivity - ViewPager2 & TabLayout 추가

--- app/src/main/java/com/example/progressiveoverload/MainActivity.kt
+++ app/src/main/java/com/example/progressiveoverload/MainActivity.kt
@@ -1,11 +1,72 @@
 package com.example.progressiveoverload
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.FrameLayout
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+import com.example.progressiveoverload.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.apply {
+            tlMain.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
+                override fun onTabSelected(tab: TabLayout.Tab?) {
+                    vpMain.currentItem = tab.position // change Position
+                }
+                override fun onTabUnselected(tab: TabLayout.Tab?) {}
+                override fun onTabReselected(tab: TabLayout.Tab?) {}
+            })
+
+            vpMain.apply {
+                adapter = PagerAdapter(supportFragmentManager, tlMain.tabCount, lifecycle)
+                registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                    override fun onPageSelected(position: Int) {
+                        tlMain.selectTab(tlMain.getTabAt(position)) // change Position
+                    }
+                })
+            }
+        }
+    }
+
+    private class PagerAdapter(
+        fragmentManager: FragmentManager,
+        val count: Int,
+        lifecycle: Lifecycle
+    ) : FragmentStateAdapter(fragmentManager, lifecycle) {
+        override fun getItemCount(): Int = count
+        override fun createFragment(position: Int): Fragment = PagerFragment(position)
+    }
+
+    class PagerFragment(private val position: Int) : Fragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            return when(position) {
+                0 -> inflater.inflate(R.layout.layout_toady_exercise, container)
+                1 -> inflater.inflate(R.layout.layout_log, container)
+                else -> inflater.inflate(R.layout.layout_toady_exercise, container)
+            }
+        }
     }
 }
